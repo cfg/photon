869642c9ca80aa849b74b16842bd3415d3e5e9fd
barry <barry@81416fe0-c9fb-472d-a4d9-98af37d240be>
2012-09-27 16:03:43 +0000

    Bump max redirects to 3.  Seems to be more reasonable based on actual usage patterns.
    git-svn-id: http://code.svn.wordpress.org/photon@218 81416fe0-c9fb-472d-a4d9-98af37d240be

--- index.php
+++ index.php
@@ -470,7 +470,7 @@ function fetch_raw_data( $url, $timeout=5, $connect_timeout=2 ) {
 	curl_setopt( $ch, CURLOPT_SSLVERSION, 3 );
 	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-	curl_setopt( $ch, CURLOPT_MAXREDIRS, 1 );
+	curl_setopt( $ch, CURLOPT_MAXREDIRS, 3 );
 	curl_setopt( $ch, CURLOPT_USERAGENT, 'Photon/1.0' );
 	return curl_exec( $ch );
 }
